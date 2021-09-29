d1e58e7a226f9dbe762f29ef99d6e5feeb23c46a
Eduardo Sohne <dudu.hit@gmail.com>
2021-09-29 17:26:57 -0300

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,81 +18,81 @@ export default function About() {
                     </Flex>
 
                     <Table colorScheme="whiteAlpha">
-                            <Thead>
-                                <Tr>
-                                    <Th px="6" color="gray.300" w="8">
-                                    </Th>
-                                    <Th>NAME / EMAIL</Th>
-                                    <Th>BIRTHDATE</Th>
-                                    <Th>CITY/STATE</Th>
-                                    <Th>PRO EXP TIME</Th>
-                                    <Th>CONDING IN REACT</Th>
-                                    <Th w="8"></Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                    <Td px="6">
-                                    </Td>
-                                    <Td>
-                                        <Box>
-                                            <Text fontWeight="bold">
-                                                Eduardo Sohne
-                                            </Text>
-                                            <Text fontSize="small" color="gray.300">
-                                                dudu.hit@gmail.com
-                                            </Text>
-                                        </Box>
-                                    </Td>
-                                    <Td>
-                                        19 de Junho, 1991
-                                    </Td>
-                                    <Td>
-                                        GRAMADO/RS
-                                    </Td>
-                                </Tr>
-                            </Tbody>
-                            <Thead>
-                                <Tr>
-                                    <Th px="6" color="gray.300" w="8">
-                                    </Th>
-                                    <Th>TECNOLOGIES THAT I WORK WITH</Th>
-                                    <Th></Th>
-                                    <Th></Th>
-                                    <Th w="8"></Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                    <Td>
-                                    </Td>
-                                    <Td>
-                                        <Box align="center">
-                                            <Icon as={SiJavascript} size="lg" />
-                                            <Text fontSize="small" color="gray.300">
-                                                JAVASCRIPT
-                                            </Text>
-                                        </Box>
-                                    </Td>
-                                    <Td>
-                                        <Box align="center">
-                                            <Icon as={SiJavascript} size="lg" />
-                                            <Text fontSize="small" color="gray.300">
-                                                REACT
-                                            </Text>
-                                        </Box>
-                                    </Td>
-                                    <Td>
-                                        <Box align="center">
-                                            <Icon as={SiJavascript} size="lg" />
-                                            <Text fontSize="small" color="gray.300">
-                                                TYPESCRIPT
-                                            </Text>
-                                        </Box>
-                                    </Td>
-                                    
-                                </Tr>
-                            </Tbody>
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" w="8">
+                                </Th>
+                                <Th>NAME / EMAIL</Th>
+                                <Th>BIRTHDATE</Th>
+                                <Th>CITY/STATE</Th>
+                                <Th>PRO EXP TIME</Th>
+                                <Th>CONDING IN REACT</Th>
+                                <Th w="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">
+                                            Eduardo Sohne
+                                        </Text>
+                                        <Text fontSize="small" color="gray.300">
+                                            dudu.hit@gmail.com
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    19 de Junho, 1991
+                                </Td>
+                                <Td>
+                                    GRAMADO/RS
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" w="8">
+                                </Th>
+                                <Th>TECNOLOGIES THAT I WORK WITH</Th>
+                                <Th></Th>
+                                <Th></Th>
+                                <Th w="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td>
+                                </Td>
+                                <Td>
+                                    <Box align="center">
+                                        <Icon as={SiJavascript} size="lg" />
+                                        <Text fontSize="small" color="gray.300">
+                                            JAVASCRIPT
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    <Box align="center">
+                                        <Icon as={SiJavascript} size="lg" />
+                                        <Text fontSize="small" color="gray.300">
+                                            REACT
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    <Box align="center">
+                                        <Icon as={SiJavascript} size="lg" />
+                                        <Text fontSize="small" color="gray.300">
+                                            TYPESCRIPT
+                                        </Text>
+                                    </Box>
+                                </Td>
+
+                            </Tr>
+                        </Tbody>
 
                     </Table>
 
