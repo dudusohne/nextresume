0ac8e62c8b1a84e0b824a7f8bf0472347dab584f
Eduardo Sohne <dudu.hit@gmail.com>
2021-09-30 20:58:58 -0300

    Update index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -16,7 +16,7 @@ export function Sidebar() {
         return (
             <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
                 <DrawerOverlay>
-                    <DrawerContent bg="gray.800" p="4">
+                    <DrawerContent bg="gray.900" p="4">
                         <DrawerCloseButton mt="6" />
                         <DrawerHeader>Menu</DrawerHeader>
                         <DrawerBody>
