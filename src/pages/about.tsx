6840e90a70dbec68a5b3b6683421d5d93df81338
Eduardo Sohne <dudu.hit@gmail.com>
2021-10-11 17:30:56 -0300

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,9 @@
-import { Box, Flex, Heading, Avatar, useColorModeValue } from '@chakra-ui/react';
+import { Box, Flex, Heading, Avatar, useColorModeValue, Stack, Text } from '@chakra-ui/react';
 import { HeaderAbout } from '../components/HeaderAbout';
 import { Sidebar } from '../components/Sidebar';
 import { AboutData } from '../components/AboutData';
-import { FaReact, FaSass } from 'react-icons/fa';
-import { SiNextDotJs, SiJavascript, SiTypescript, SiStyledComponents, SiCss3, SiHtml5 } from 'react-icons/si';
+import { FaReact, FaSass, FaGitAlt } from 'react-icons/fa';
+import { SiNextDotJs, SiJavascript, SiTypescript, SiStyledComponents, SiCss3, SiHtml5, SiStripe, SiPrismic } from 'react-icons/si';
 
 export default function About() {
     const color = useColorModeValue("gray.200", "gray.800")
@@ -23,15 +23,33 @@ export default function About() {
                     </Flex>
                     <Flex direction="row">
                         <AboutData />
-                        <Box bg={bg} maxWidth={540} borderRadius={12} p="4">
-                            <FaReact />
-                            <SiNextDotJs />
-                            <SiJavascript />
-                            <SiTypescript />
-                            <SiStyledComponents />
-                            <FaSass />
-                            <SiCss3 />
-                            <SiHtml5 />
+                        <Box bg={bg} maxWidth={540} borderRadius={12} p="4" ml="4">
+                            <Text fontSize={12} mb="5">TECNOLOGIES</Text>
+                            <Stack spacing="3">
+                                <Flex direction="row">
+                                    <Stack spacing="2" direction="row">
+                                        <FaReact size={30} />
+                                        <SiNextDotJs size={30} />
+                                        <SiJavascript size={30} />
+                                        <SiTypescript size={30} />
+                                    </Stack>
+                                </Flex>
+                                <Flex direction="row">
+                                    <Stack spacing="2" direction="row">
+                                        <SiStyledComponents size={30} />
+                                        <FaSass size={30} />
+                                        <SiCss3 size={30} />
+                                        <SiHtml5 size={30} />
+                                    </Stack>
+                                </Flex>
+                                <Flex direction="row">
+                                    <Stack spacing="2" direction="row">
+                                        <SiStripe size={30} />
+                                        <SiPrismic size={30} />
+                                        <FaGitAlt size={30} />
+                                    </Stack>
+                                </Flex>
+                            </Stack>
                         </Box>
                     </Flex>
                 </Box>
