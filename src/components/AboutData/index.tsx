114766b821fe1d10539a8bec1febf7c8da3530e2
Eduardo Sohne <dudu.hit@gmail.com>
2021-10-11 13:59:59 -0300

    update about grid

--- src/components/AboutData/index.tsx
+++ src/components/AboutData/index.tsx
@@ -1,28 +1,26 @@
 import { Text, useColorModeValue, Grid, GridItem, Box } from '@chakra-ui/react';
 
 export function AboutData() {
-    const bg = useColorModeValue("red.500", "red.200")
-    const color = useColorModeValue("gray.800", "gray.200")
-    const border = useColorModeValue("gray.200", "gray.300")
-
+    const color = useColorModeValue("gray.200", "gray.800")
+    const color2 = useColorModeValue("gray.800", "gray.200")
     return (
-        <Box bg={border} maxWidth={540}>
+        <Box bg={color} maxWidth={540} borderRadius={12} p="4">
             <Grid
-                templateRows="repeat(3, 1fr)"
-                templateColumns="repeat(4, 1fr)"
+                templateRows="repeat(2, 1fr)"
+                templateColumns="repeat(2, 1fr)"
                 gap={3}
             >
-                <GridItem colSpan={1} bg="red">
-                    <Text as="span" size="sm" fontWeight="bold">Fulltime Job:<Text as="p" fontSize={22} fontWeight="normal">React Developer</Text></Text>
+                <GridItem colSpan={1} rowSpan={1}>
+                    <Text as="span" size="sm" fontWeight="bold" color={color2}>Fulltime Job:<Text as="p" fontSize={22} fontWeight="normal">React Developer</Text></Text>
                 </GridItem>
-                <GridItem colSpan={2} bg="blue">
-                    <Text as="span">Company:<Text as="p">Smart Marketing Digital</Text></Text>
+                <GridItem colSpan={1} rowSpan={1}>
+                    <Text as="span">Company:<Text as="p" fontSize={22} color={color2}>Smart Marketing Digital</Text></Text>
                 </GridItem>
-                <GridItem colSpan={3}>
-                    <Text as="span">Period:<Text as="p">03/2021 / Present</Text></Text>
+                <GridItem rowSpan={1} colSpan={1}>
+                    <Text as="span">Period:<Text as="p" color={color2}>03/2021 / Present</Text></Text>
                 </GridItem>
-                <GridItem colSpan={4}>
-                    <Text as="span">Role:<Text as="p">Web Developer</Text></Text>
+                <GridItem rowSpan={1} colSpan={1}>
+                    <Text as="span">Role:<Text as="p" color={color2}>Web Developer</Text></Text>
                 </GridItem>
             </Grid>
         </Box>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Avatar, Icon, Table, Thead, Tr, Th, Stack, Tbody, Td, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Avatar, Icon, Table, Thead, Tr, Th, Stack, Tbody, Td, Text, useColorModeValue } from '@chakra-ui/react';
 import { HeaderAbout } from '../components/HeaderAbout';
 import { Sidebar } from '../components/Sidebar';
 import { SiJavascript } from 'react-icons/si';
@@ -6,6 +6,8 @@ import { Pagination } from '../components/Pagination';
 import { AboutData } from '../components/AboutData';
 
 export default function About() {
+    const color = useColorModeValue("gray.200", "gray.800")
+    const color2 = useColorModeValue("gray.800", "gray.200")
 
     return (
         <Box>
@@ -13,9 +15,9 @@ export default function About() {
 
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
-                <Box flex="1" borderRadius={8} bg="gray.100" p="8" borderWidth={1} borderColor="gray.400">
+                <Box flex="1" borderRadius={8} p="8" borderWidth={1} borderColor="gray.400" bg={color2}>
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal" color="black">About Me</Heading>
+                        <Heading size="lg" fontWeight="normal" color={color}>About Me</Heading>
                         <Avatar size="lg" name="Eduardo Sohne" src="https://avatars.githubusercontent.com/u/19408694?v=4" />
                     </Flex>
                     <AboutData />
